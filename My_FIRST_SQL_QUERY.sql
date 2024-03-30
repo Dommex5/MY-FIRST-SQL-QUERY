3769dcbabe78dd91491fd518238152c170f263aa
Dommex5 <dommex5@gmail.com>
2024-03-30 05:52:25 +0100

    Create My_FIRST_SQL_QUERY.sql

---
+++ My_FIRST_SQL_QUERY.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE  Assignment;
+USE Assignment;
+CREATE TABLE Today_Resources (
+Age int,
+Job varchar(30),
+Marital varchar(30),
+Education varchar(30),
+_Default varchar(30),
+Balance int,
+Housing varchar(30),
+Loan varchar(30),
+Contact varchar(30),
+Day int,
+Month varchar(30),
+Duration int,
+Campaign int,
+Pdays int,
+Previous int,
+Poutcome varchar(30),
+Y varchar(30));
+select * From Today_Resources;
